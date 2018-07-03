49e4364e5e34db1d033910425cc33b97908fa8aa
Jaimemosg <37384444+Jaimemosg@users.noreply.github.com>
2018-07-02 20:43:10 -0500

    Parámetro sigma
    Sustitución del parámetro \theta por \sigma en líneas donde el cambio estaba pendiente.

--- R/OW.R
+++ R/OW.R
@@ -19,10 +19,10 @@
 #' P[X <= x], otherwise, P[X > x].
 #' 
 #' @details 
-#' The generalized power weibull with parameters \code{mu}, \code{theta}
+#' The generalized power weibull with parameters \code{mu}, \code{sigma}
 #' and \code{nu} has density given by
 #' 
-#' f(x) = mu*theta*nu*x^(sigma-1)*exp(mu*(x^sigma))*(exp(mu*(x^sigma))-1)^(nu-1)*(1+(exp(mu*(x^sigma))-1)^nu)^-2
+#' f(x) = mu*sigma*nu*x^(sigma-1)*exp(mu*(x^sigma))*(exp(mu*(x^sigma))-1)^(nu-1)*(1+(exp(mu*(x^sigma))-1)^nu)^-2
 #'
 #' for x > 0.
 #' 
@@ -34,24 +34,24 @@
 #' @export
 #' @examples  
 #' ## The probability density function 
-#' curve(dOW(x, mu = 2, theta = 3, nu = 0.2), from = 0, to = 4, ylim = c(0, 2), col = "red", las = 1, ylab = "The probability density function")
+#' curve(dOW(x, mu = 2, sigma = 3, nu = 0.2), from = 0, to = 4, ylim = c(0, 2), col = "red", las = 1, ylab = "The probability density function")
 #' 
 #' ## The cumulative distribution and the Reliability function
 #' par(mfrow = c(1, 2))
-#' curve(pOW(x, mu = 2, theta = 3, nu = 0.2), from = 0, to = 4, ylim = c(0, 1), col = "red", las = 1, ylab = "The cumulative distribution function")
-#' curve(pOW(x, mu = 2, theta = 3, nu = 0.2, lower.tail = FALSE), from = 0, to = 4,  ylim = c(0, 1), col = "red", las = 1, ylab = "The Reliability function")
+#' curve(pOW(x, mu = 2, sigma = 3, nu = 0.2), from = 0, to = 4, ylim = c(0, 1), col = "red", las = 1, ylab = "The cumulative distribution function")
+#' curve(pOW(x, mu = 2, sigma = 3, nu = 0.2, lower.tail = FALSE), from = 0, to = 4,  ylim = c(0, 1), col = "red", las = 1, ylab = "The Reliability function")
 #' 
 #' ## The quantile function
 #' p <- seq(from = 0, to = 0.998, length.out = 100)
-#' plot(x = qOW(p, mu = 2, theta = 3, nu = 0.2), y = p, xlab = "Quantile", las = 1, ylab = "Probability")
-#' curve(pOW(x, mu = 2, theta = 3, nu = 0.2), from = 0, add = TRUE, col = "red")
+#' plot(x = qOW(p, mu = 2, sigma = 3, nu = 0.2), y = p, xlab = "Quantile", las = 1, ylab = "Probability")
+#' curve(pOW(x, mu = 2, sigma = 3, nu = 0.2), from = 0, add = TRUE, col = "red")
 #' 
 #' ## The random function
-#' hist(rOW(n = 10000, mu = 2, theta = 3, nu = 0.2), freq = FALSE, ylim = c(0, 2),xlab = "x", las = 1, main = "")
-#' curve(dOW(x, mu = 2, theta = 3, nu = 0.2),  from = 0, ylim = c(0, 2), add = T, col = "red")
+#' hist(rOW(n = 10000, mu = 2, sigma = 3, nu = 0.2), freq = FALSE, ylim = c(0, 2),xlab = "x", las = 1, main = "")
+#' curve(dOW(x, mu = 2, sigma = 3, nu = 0.2),  from = 0, ylim = c(0, 2), add = T, col = "red")
 #' 
 #' ## The Hazard function
-#' curve(hOW(x, mu = 2, theta = 3, nu = 0.2), from = 0, to = 2.5, ylim = c(0, 3), col = "red", ylab = "The hazard function", las = 1)
+#' curve(hOW(x, mu = 2, sigma = 3, nu = 0.2), from = 0, to = 2.5, ylim = c(0, 3), col = "red", ylab = "The hazard function", las = 1)
 OW <- function (mu.link = "log", sigma.link = "log", nu.link = "log") 
 {
   mstats <- checklink("mu.link",    "Odd Weibull", substitute(mu.link),    c("log", "own"))
